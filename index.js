f0bfce569ba1f61d2d7d0e5406c3e7673fc4efd4
mutoukouta <smutou@nssx.co.jp>
2023-09-13 15:56:15 +0900

    09/13

--- index.js
+++ index.js
@@ -1,69 +1,58 @@
 import data from "./data.json" assert { type: "json" };
 
-//ボタンの出力
+const recipeTbody = document.querySelector("#recipeDisplay tbody");
+const recipeList = document.getElementById("recipeList");
+
+//テーブルの出力
 for (let i = 0; i < data.length; i++) {
-  let recipeItem = document.createElement("button"); // li要素を作成
-  let textNode = document.createTextNode(data[i].name); // テキストノードを作成
-  recipeItem.appendChild(textNode); // テキストノードをli要素に追加
-  let recipeButton = document.getElementById("buttons"); // ul要素を取得
-  recipeButton.appendChild(recipeItem);
-}
+  let recipeListCloned = recipeList.cloneNode(true);
+  let recipeName = recipeListCloned.querySelector("#recipeName");
+  let recipeDesc = recipeListCloned.querySelector("#recipeDesc");
+  let recipeCreator = recipeListCloned.querySelector("#recipeCreator");
+  let recipeCalorie = recipeListCloned.querySelector("#recipeCalorie");
+  let recipeTime = recipeListCloned.querySelector("#recipeTime");
 
-//レシピボタンが押下後
-function list(num) {
-  let recipeList = document.createElement("li");
+  recipeName.innerText = data[i].name;
+  recipeDesc.innerText = data[i].desc;
+  recipeCreator.innerText = data[i].creator;
+  recipeCalorie.innerText = data[i].calorie;
+  recipeTime.innerText = data[i].time;
 
-  let recipeName = document.createTextNode(
-    "recipeName:" + data[num].name + "/"
-  );
-  let recipeDesc = document.createTextNode(
-    "recipeDesc:" + data[num].desc + "/"
-  );
-  let recipeCreator = document.createTextNode(
-    "recipeCreator:" + data[num].creator + "/"
-  );
-  let recipeCalorie = document.createTextNode(
-    "recipeCalorie:" + data[num].calorie + "/"
-  );
-  let recipeTime = document.createTextNode(
-    "recipeTime:" + data[num].time + "/"
-  );
-  recipeList.appendChild(recipeName);
-  recipeList.appendChild(recipeDesc);
-  recipeList.appendChild(recipeCreator);
-  recipeList.appendChild(recipeCalorie);
-  recipeList.appendChild(recipeTime);
-  let recipeDisplay = document.getElementById("recipeDisplay");
-  recipeDisplay.appendChild(recipeList);
+  recipeTbody.appendChild(recipeListCloned);
 }
-const buttons = document.querySelectorAll("#buttons button");
-let buttonLists = [].slice.call(buttons);
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    list(buttonLists.indexOf(btn));
-  });
-});
 
 const searchButton = document.getElementById("searchButton");
 searchButton.addEventListener("click", () => {
   const search = document.getElementById("search").value;
   let searchArr = search.split(/[\s\u3000]+/);
-  let dataArr = [];
-
+ 
   for (let i = 0; i < data.length; i++) {
+    //for文でjsonファイルの値を一つ取得
     let dataList = data[i];
+
     let check = true;
+    //変数tmpでjsonファイルからnameとdescの値（文字列）を取得
     let tmp = dataList.name + dataList.desc;
+    //for文で変数searchArrの要素分だけ繰り返すよう命令
+    //①例えば、変数searchArrの中身が「なす　野菜」だった場合、最初に「なす」があるかを検知する
     for (let a = 0; a < searchArr.length; a++) {
+      //変数tmpの元々の値とreplaceメソッドで変数searchArrに入力された文字を削った値を比較
+      //下記のif文がtrueになる場合、変数tmpと削った変数tmpが同じ値の為、検索に引っかからないので処理を終了させる
+      //falseになる場合、それぞれの値に相違があるのでif文の中身はスキップされる
       if (tmp === tmp.replace(searchArr[a], "")) {
         check = false;
         break;
       }
+      //②ここの部分で「なす」を削る、まだ「野菜」が残っているので、二つ目のfor文に戻る
       tmp = tmp.replace(searchArr[a], "");
     }
     if (check) {
-      dataArr.push(dataList);
+      let recipeList = document.querySelectorAll("#recipeList")[i + 1];
+      recipeList.style.backgroundColor = "red";
+    } else {
+      let recipeList = document.querySelectorAll("#recipeList")[i + 1];
+      recipeList.style.backgroundColor = null;
     }
+    
   }
-  console.log(dataArr);
 });
